ade53a7ed6670ba38a5a4a4bedd850992e40710f
LAB13_USER ON WS03 <L13Y12W03@citulab.net>
2025-03-25 14:52:44 +0800

    added paint to circle

--- src/main/java/com/csit228/graphstabada/Thing.java
+++ src/main/java/com/csit228/graphstabada/Thing.java
@@ -2,6 +2,10 @@ package com.csit228.graphstabada;
 
 import javafx.scene.Node;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 
@@ -9,7 +13,12 @@ public class Thing extends Node {
     StackPane sp;
 
     public Thing(String text,double x_val, double y_val) {
-        Circle circle = new Circle();
+        LinearGradient paint = new LinearGradient(
+                0.0, 0.3318, 1.0, 1.0, true, CycleMethod.NO_CYCLE,
+                new Stop(0.0, new Color(1.0, 0.0, 0.0, 0.729)),
+                new Stop(1.0, new Color(1.0, 1.0, 1.0, 1.0)));
+
+        Circle circle = new Circle(33.0,paint);
         Text name = new Text(text);
         sp = new StackPane(circle,name);
 
